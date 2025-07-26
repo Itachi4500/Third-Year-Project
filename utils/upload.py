a5988057af2e48a8d9acc5f61d07037a866d524a
Itachi4500 <manojmore0214@gmail.com>
2025-07-26 22:50:35 +0530

    Update upload.py

--- utils/upload.py
+++ utils/upload.py
@@ -1,20 +1,20 @@
-import streamlit as st
-import pandas as pd
+# import streamlit as st
+# import pandas as pd
 
-def upload_data(uploaded_file):
-    try:
-        file_name = uploaded_file.name
-        if file_name.endswith(".csv"):
-            df = pd.read_csv(uploaded_file)
-        elif file_name.endswith(".xlsx"):
-            df = pd.read_excel(uploaded_file)
-        else:
-            st.warning("⚠️ Please upload a valid CSV or Excel file.")
-            return None
+# def upload_data(uploaded_file):
+#     try:
+#         file_name = uploaded_file.name
+#         if file_name.endswith(".csv"):
+#             df = pd.read_csv(uploaded_file)
+#         elif file_name.endswith(".xlsx"):
+#             df = pd.read_excel(uploaded_file)
+#         else:
+#             st.warning("⚠️ Please upload a valid CSV or Excel file.")
+#             return None
 
-        st.session_state.uploaded_df = df
-        st.success("✅ File uploaded successfully!")
-        return df
-    except Exception as e:
-        st.error(f"❌ Error reading file: {e}")
-        return None
+#         st.session_state.uploaded_df = df
+#         st.success("✅ File uploaded successfully!")
+#         return df
+#     except Exception as e:
+#         st.error(f"❌ Error reading file: {e}")
+#         return None
